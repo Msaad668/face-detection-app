cb5afc78e1880574cc837f5c1e04a6665612cc1b
unknown <mostafasaad55@gmail.com>
2020-05-06 05:08:21 +0200

    bug fixed

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ class App extends Component {
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
         />
-        {route === "signin" ? (
+        {route === "home" ? (
           <div>
             <Logo />
             <Rank
@@ -134,7 +134,7 @@ class App extends Component {
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-        ) : route === "home" ? (
+        ) : route === "signin" ? (
           <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
           <Register
